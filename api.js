e3f86eaf6fea55b4da11500804873edc46d765f1
Adibab <adiba.du.bd@gmail.com>
2022-12-24 00:43:24 +0000

    bugs fixed

--- api.js
+++ api.js
@@ -9,7 +9,6 @@ const selectEpisode = document.querySelector("#select-episode");
 const title = document.querySelector(".title");
 const goBackBtn = document.querySelector("#goBackBtn");
 let allEpisodes;
-console.log(allEpisodes);
 const allShows = getAllShows();
 
 function loadShowOnPage(allShows) {
@@ -32,16 +31,14 @@ function loadShow(shows) {
     const paragraph = document.createElement("p");
     const titleDiv = document.createElement("div");
     const newbody = document.createElement("div");
-    //  adding episod name
+
     title.innerText = `${show.name}`;
-    // title.appendChild(span)
-    // span.innerText = `S0${show.season}E0${show.number}`;
+
     titleDiv.appendChild(title);
-    //  titleDiv.appendChild(span);
+
     newbody.appendChild(titleDiv);
     header.appendChild(newbody);
-    // episod image
-    // console.log(show.image);
+
     image.src =
       show.image == null
         ? "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/summer-flowers-star-flower-1648071187.jpg"
@@ -50,46 +47,26 @@ function loadShow(shows) {
     newbody.appendChild(image);
     header.appendChild(newbody);
     image.classList.add("img");
-    // paragraph
+
     paragraph.innerHTML = show.summary;
-    paragraph.classList.add("movie-summary")
+    paragraph.classList.add("movie-summary");
     newbody.appendChild(paragraph);
-    header.appendChild(newbody);
+
     newbody.classList.add("content");
     titleDiv.classList.add("title-class");
     paragraphforSearch.innerHTML = `Displaying ${allShows.length} / ${allShows.length} Movie Shows`;
-    // console.log(paragraphforSearch)
+
+    const wrapperLink = document.createElement("a");
+    wrapperLink.classList.add("content");
+
+    wrapperLink.target = "_blank";
+    wrapperLink.appendChild(newbody);
+    header.appendChild(wrapperLink);
+
+    wrapperLink.addEventListener("click", () => {
+      fetchEpisodes(show.id);
+    });
   }
-}
-
-//  got episode pages by selecting each show
-content.addEventListener("change", (e) => {
-
-})
-
-// search button to show specific shows
-function searchShows(allShows) {
-  searchBox.addEventListener("keyup", (e) => {
-    // paragraphforSearch.classList.toggle("hidden");
-    const value = e.target.value.toLowerCase();
-    // console.log(value);
-    // console.log(allShows);
-    const remainingEpisodes = allShows.filter(
-      (episode) =>
-        episode.name.toLowerCase().includes(value) ||
-        episode.summary.toLowerCase().includes(value)
-    );
-    // console.log(remainingEpisodes);
-    header.innerHTML = "";
-    loadShow(remainingEpisodes);
-    console.log(remainingEpisodes);
-
-    if (value) {
-      paragraphforSearch.innerHTML = `Displaying ${remainingEpisodes.length}/ ${allShows.length} episodes`;
-    } else {
-      paragraphforSearch.innerHTML = " ";
-    }
-  });
 }
 
 //  level-400
@@ -107,13 +84,12 @@ selectMovies(allShows);
 
 //  selecting episode & showing episode accoridngly
 selectShow.addEventListener("change", function () {
-  // console.log(selectEpisode);
   if (selectShow.value === "none") {
     loadShowOnPage(allShows);
     selectEpisode.innerHTML = "Episodes";
-    // console.log(selectEpisode);
   } else {
     const showId = allShows.find((show) => show.name === selectShow.value).id;
+
     getTheEpisodesList(showId);
   }
 });
@@ -129,23 +105,22 @@ function fetchEpisodes(id) {
   fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
     .then((res) => res.json())
     .then((data) => {
-      // console.log(data)
       allEpisodes = data;
-      //  console.log(allEpisodes)
+
       header.innerHTML = " ";
-      // function for loading episode in the body
+
       loadEpisodes(allEpisodes);
-      // for episode option
-      selectEpisode.innerHTML = "Episodes";
-      episodeOption(allEpisodes);
+
+      selectEpisode.innerHTML = " ";
+    
+       episodeOption(allEpisodes);
       allEpisodes.forEach((episode) => {
         let option = document.createElement("option");
         option.innerText = `S0${episode.season}E0${episode.number} - ${episode.name}`;
         selectEpisode.appendChild(option);
-        // paragraphforSearch.classList.toggle("hidden");
+
         paragraphforSearch.innerHTML = `Displaying ${allEpisodes.length} episodes`;
-        // console.log(paragraphforSearch)
-        //
+
       });
 
       searchEpisodesList(allEpisodes);
@@ -154,7 +129,6 @@ function fetchEpisodes(id) {
 
 // function for loading episode in the body
 function loadEpisodes(allEpisodes) {
-  // const wrapperLink = document.createElement("a");
   for (let episode of allEpisodes) {
     const title = document.createElement("h2");
     const span = document.createElement("span");
@@ -172,7 +146,7 @@ function loadEpisodes(allEpisodes) {
     paragraph.innerHTML = episode.summary;
 
     span.innerHTML = `<a href = ${episode.url}>Find more</a>`;
-    console.log(span)
+  
 
     titleDiv.appendChild(title);
 
@@ -193,18 +167,79 @@ function loadEpisodes(allEpisodes) {
 function episodeOption() {
   selectEpisode.value = " ";
   const optionEl = document.createElement("option");
-  optionEl.innerText = "Episodes";
+   optionEl.innerText = "Episodes";
   selectEpisode.appendChild(optionEl);
+
   fetchEpisodes(allEpisodes);
   loadEpisodes(allEpisodes);
+}
+
+// level 500
+// selecting episode from the drop down menu  & generating  episode in the body
+
+selectEpisode.addEventListener("change", function () {
+  const value = selectEpisode.value.slice(9);
+
+  let remainingEpisodes = allEpisodes.filter(
+    (episode) => episode.name === value
+  );
+
+  if (value) {
+    paragraphforSearch.innerHTML = `Displaying ${remainingEpisodes.length}/ ${allEpisodes.length} episodes`;
+  } else {
+    paragraphforSearch.innerHTML = " ";
+  }
+  searchBox.value = "";
+  header.innerHTML = "";
+
+  loadEpisodes(remainingEpisodes);
+
+  goBackBtn.addEventListener("click", gobackFromShowPage);
+});
+
+// add event listner to goback btn to go back from movie show list page
+goBackBtn.addEventListener("click", gobackFromShowPage);
+
+function gobackFromShowPage() {
+  header.innerHTML = " ";
+  loadShow(allShows);
+  selectShow.value = "none";
+  selectEpisode.innerText = "Episodes";
+  paragraphforSearch.innerHTML = "Displaying 301 episodes";
+  searchBox.value = "";
+}
+
+
+
+// search button+++++++++++++++++++
+
+// search button to show specific shows
+function searchShows(allShows) {
+  searchBox.addEventListener("keyup", (e) => {
+    const value = e.target.value.toLowerCase();
+
+    const remainingEpisodes = allShows.filter(
+      (episode) =>
+        episode.name.toLowerCase().includes(value) ||
+        episode.summary.toLowerCase().includes(value)
+    );
+
+    header.innerHTML = "";
+    loadShow(remainingEpisodes);
+    console.log(remainingEpisodes);
+
+    if (value) {
+      paragraphforSearch.innerHTML = `Displaying ${remainingEpisodes.length}/ ${allShows.length} episodes`;
+    } else {
+      paragraphforSearch.innerHTML = " ";
+    }
+  });
 }
 
 // search button to show specific episodes
 
 function searchEpisodesList(allEpisodes) {
   searchBox.addEventListener("keyup", (e) => {
-    // paragraphforSearch.classList.toggle("hidden");
-
     const value = e.target.value.toLowerCase();
     const remainingEpisods = allEpisodes.filter(
       (episode) =>
@@ -222,42 +257,4 @@ function searchEpisodesList(allEpisodes) {
       paragraphforSearch.innerHTML = " ";
     }
   });
-}
-
-// level 500
-// selecting episode from the drop down menu  & generating  episode in the body
-
-selectEpisode.addEventListener("change", function () {
-  const value = selectEpisode.value.slice(9);
-  // console.log(value);
-  let remainingEpisodes = allEpisodes.filter(
-    (episode) => episode.name === value
-  );
-  // console.log(remainingEpisodes);
-  // if (!value) {
-  //   remainingEpisodes = allEpisodes;
-  // }
-  if (value) {
-    paragraphforSearch.innerHTML = `Displaying ${remainingEpisodes.length}/ ${allEpisodes.length} episodes`;
-  } else {
-    paragraphforSearch.innerHTML = " ";
-  }
-  searchBox.value = "";
-  header.innerHTML = "";
-  // paragraphforSearch.innerHTML = "";
-  loadEpisodes(remainingEpisodes);
-  // goBackBtn.addEventListener("click" , gobackFromEpisode)
-  goBackBtn.addEventListener("click", gobackFromShowPage);
-});
-
-// add event listner to goback btn to go back from movie show list page
-goBackBtn.addEventListener("click", gobackFromShowPage);
-
-function gobackFromShowPage() {
-  header.innerHTML = " ";
-  loadShow(allShows);
-  selectShow.value = "none";
-  selectEpisode.innerText = "Episodes";
-  paragraphforSearch.innerHTML = "Displaying 301 episodes";
-  searchBox.value = "";
 }
